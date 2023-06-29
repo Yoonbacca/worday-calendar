914b338c5870776f3c9408ef12e90d47dbc6cabf
Yoonbacca <yoonjaekim3@gmail.com>
2023-06-29 15:14:33 -0500

    Added save button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,38 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// the code isn't run until the browser has finished rendering
 $(function () {
-  // Issue 1: Display current date at the top of the page
+  // Declare two constant variables, today and currentHour. Whole function scope
   const today = dayjs().format('MMM D, YYYY');
   const currentHour = Number(dayjs().format('H'));
-  console.log(currentHour);
-  $('#currentDay').text("Current Day: " + today);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  // For loop to update the color coding of each time block
+  // Applies current date to top of page
+  $('#currentDay').text(today);
+
+  // Add click listener to all button elements aria label 'save'. 
+  $('button[aria-label="save"]').on( "click", function() {
+    // Find the <textarea> element using the 'this' selector and the 'siblings' method.
+    let textArea = $(this).siblings('textarea');
+
+    // Get the user input from the text area
+    let userInput = JSON.stringify(textArea.val());
+
+    // The 'closest' method traverses up the DOM tree, then the 'attr' method retrieves the id of the parent
+    let timeBlockId = textArea.closest('.time-block').attr('id');
+
+    // Save the user input in local storage
+    localStorage.setItem(timeBlockId, userInput);
+  } );
+
+  // Run through each possible hour id in each time block
   for (var i = 9; i < 18; i++) {
+    // Declare a string to represent hour id (e.g. '#hour-10')
     let hour = "#hour-" + i.toString();
-    console.log(hour);
-    console.log(i);
+
+    // Declare a variable to store parsed text retrieved from local storage
+    // Substring used to remove the hashtag
+    let savedText = JSON.parse(localStorage.getItem(hour.substring(1)));
+
+    // If/else condition to assign appropriate color classes depending on the hour
     if (i === currentHour) {
       $(hour).addClass('present').removeClass('past').removeClass('future');
     } else if (i < currentHour) {
@@ -26,6 +40,8 @@ $(function () {
     } else if (i > currentHour) {
       $(hour).addClass('future').removeClass('present').removeClass('past');
     }
+
+    $(hour).children("textarea").text(savedText);
   }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
